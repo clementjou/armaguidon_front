c25c384e27f864d245f47d074f0275887ca55461
Jouanneau Clément <jouanneau.c@gmail.com>
2018-12-21 11:42:20 +0100

    modifs

--- src/App.js
+++ src/App.js
@@ -2,66 +2,19 @@ import React, { Component } from 'react';
 import getDataDashboard from './api/api';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Login } from './login';
 
 import './App.css';
 
 
-export default class App extends Component {
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <div>Coucou</div>
+      <Route  path="/" Component={Login} />
+    </Switch>
+  </BrowserRouter>
+)
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      userName: ""
-    }
-
-    this.getDashboard = this.getDashboard.bind(this);
-  }
-
-  onFormChange(value) {
-    this.setState({
-      userName: value
-    })
-  }
-
-
-
-  getDashboard(arg) {
-    arg.preventDefault();
-    let username = this.state.userName;
-    if (username) {
-      getDataDashboard(username).then(res => {
-        return res.data && res.data.map((res) => {
-          if (res.Firstname && res.Firstname.toLowerCase() == username && username.toLowerCase()) {
-            this.setState({ currentUser: this.state.userName, noUserFound: false });
-          } else {
-            this.setState({ noUserFound: true, currentUser: "" })
-          }
-        })
-      })
-    }
-  }
-
-  render() {
-    let User;
-    if (this.state.noUserFound) {
-      User = <div className="error">
-        No user found
-      </div>
-    }
-    if (this.state.currentUser) {
-      User = <div className="login-message">
-        Hello {this.state.currentUser}
-      </div>
-    }
-
-    return (
-      <div className="App">
-        <form onSubmit={(arg) => this.getDashboard(arg)} className="connexion-form">
-          <TextField className="login-input" error={this.state.noUserFound} onChange={(arg) => this.onFormChange(arg.target.value)} type="text" placeholder="Type your name" value={this.state.userName} />
-          <Button variant="contained" onClick={this.getDashboard}>Valider</Button>
-        </form>
-        {User}
-      </div>
-    );
-  }
-}
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
-class Client extends React.Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <Switch>
-                    <div>Coucou</div>
-                    <Route path="/home" Component={App} />
-                </Switch>
-            </BrowserRouter>
-        )
-    }
-}
-ReactDOM.render(<Client />, document.getElementById('root'));
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
