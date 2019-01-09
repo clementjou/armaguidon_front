b56245572f79016afd4a993e5e2310fb61069c76
Jouanneau Clément <jouanneau.c@gmail.com>
2019-01-09 13:56:34 +0100

    init service manager

--- src/app/services/servicecatalog.tsx
+++ src/app/services/servicecatalog.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
+import Popover from '../popover/index';
+import ServiceManager from '../services/servicemanager';
 
-interface ServiceCatalogListProps{
+interface ServiceCatalogListProps {
 
 }
 
 export default class ServiceCatalogList extends React.Component<ServiceCatalogListProps, any>{
 
-    render(){
+    render() {
         let services;
         const catalog = require('../services/services.json');
-        if(catalog && catalog.services && catalog.services.length){
-            services = catalog.services.map((s, i)=>{
+        if (catalog && catalog.services && catalog.services.length) {
+            services = catalog.services.map((s, i) => {
                 return <CatalogItem item={s} key={i} />
             })
         }
@@ -22,13 +24,33 @@ export default class ServiceCatalogList extends React.Component<ServiceCatalogLi
 
 
 interface CatalogItemProps {
-item: any;
+    item: any;
 }
 
-class CatalogItem extends React.Component<CatalogItemProps,any>{
-    render(){
+class CatalogItem extends React.Component<CatalogItemProps, any>{
+    constructor(props) {
+        super(props);
+        this.editService = this.editService.bind(this);
+        this.state={
+            showPopover: false
+        }
+    }
+    onClose() {
+
+    }
+
+    editService() {
+        this.setState({showPopover: true})
+    }
+
+    render() {
+        let props = {
+            type: this.props.item.id
+        }
         return <div className="catalog-item">
+            <button onClick={() => this.editService()}></button>
             {this.props.item.id}
+            {this.state.showPopover ? <Popover onClose={this.onClose} component={ServiceManager} customProps={props} />: null}
         </div>;
     }
 }
---
+++ src/app/services/servicemanager.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface ServiceManagerProps{
+    type: string
+}
+
+export default class ServiceManager extends React.Component<ServiceManagerProps,any>{
+    render(){
+        let component;
+        return 'a'
+    }
+}
