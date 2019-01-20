3f6c7a1179ff7660c6c3ac1c6e81cef615ad04fb
Jouanneau Clément <cjouanneau@infinitesquare.com>
2019-01-20 20:02:51 +0100

    some modifications to weaher displayer

--- src/app/services/displayers/weatherservice.displayer.tsx
+++ src/app/services/displayers/weatherservice.displayer.tsx
@@ -1,31 +1,45 @@
 import * as React from 'react';
-import {getWeatherApi} from "../../../api/api";
+import { getWeatherApi } from "../../../api/api";
 
-interface WeatherServiceDisplayerProps{
-item: any;
+interface WeatherServiceDisplayerProps {
+    item: any;
 }
 
-export class WeatherServiceDisplayer extends React.Component<WeatherServiceDisplayerProps,any>{
-    constructor(props){
+export class WeatherServiceDisplayer extends React.Component<WeatherServiceDisplayerProps, any>{
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             weather: null
         }
     }
-    componentDidMount(){
-        getWeatherApi(this.props.item && this.props.item.config && this.props.item.config.location).then((weather)=>{
-            if(weather){
-                this.setState({weather});
+
+    getWeatherImage(condition) {
+        if (condition) {
+            switch (condition) {
+                case "mist":
+                    return require("../../../assets/images/weather/sunny.jpg");
+            }
+        }
+    }
+
+
+    componentDidMount() {
+        getWeatherApi(this.props.item && this.props.item.config && this.props.item.config.location).then((weather) => {
+            if (weather) {
+                this.setState({ weather });
             }
         })
     }
 
-    render(){
+    render() {
         let cityName = this.state.weather && this.state.weather.name;
         let temp = this.state.weather && this.state.weather.main && this.state.weather.main.temp;
-        return <div className="weather-service-displayer">
+        let condition = this.state.weather && this.state.weather.weather && this.state.weather.weather[0] && this.state.weather.weather[0].description;
+        let image = this.getWeatherImage(condition);
+        return <div className="weather-service-displayer" style={{ backgroundImage: `url(${image})` }}>
             <p>{cityName}</p>
-            <span>{temp}</span>
+            <p>{condition}</p>
+            <p>{temp} °F</p>
         </div>
     }
 }
